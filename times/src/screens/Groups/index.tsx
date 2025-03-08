e86d51bb9590f4a5303acf51f9addc5b73aa03be
Leonardo Pedroso <116748167+PedrosoleoSilva@users.noreply.github.com>
2025-03-08 17:44:27 -0300

    adicionando logo do projeto

--- times/src/screens/Groups/index.tsx
+++ times/src/screens/Groups/index.tsx
@@ -1,11 +1,19 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 const Groups = () => {
     return (
-        <View>
+        <View style={styles.container}>
             <Text>Groups</Text>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
+})
 
 export default Groups;
